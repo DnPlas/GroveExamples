da6691c2c51c70eb20906434280d6faa4d1b182b
Daniela Plascencia <daplascen@gmail.com>
2016-11-02 20:10:15 +0000

    Adds servo example
    Signed-off-by: Daniela Plascencia <daplascen@gmail.com>

---
+++ servo.py
@@ -0,0 +1,18 @@
+import time
+import pyupm_servo as s
+import pyupm_grove as g
+
+servo = s.ES08A(5)
+servo.setAngle(30)
+temp = g.GroveTemp(0)
+print 'Setting servo at home position'
+time.sleep(1)
+
+if __name__ == '__main__':
+        while True:
+            angle = (temp.value()*24)-570
+            if angle > 180 or angle < 0:
+                servo.setAngle(30)
+            else:
+                servo.setAngle(angle)
+del servo
